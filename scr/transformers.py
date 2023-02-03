84dbb850389f953215945deaf6d2a00e61f1acdd
WinnieCP <winnie.poel@gmail.com>
2023-02-03 23:46:49 +0100

    time lagged features added, work well for san juan, notebook 05 shows different correlations for iquitos, might need differently lagged featurs

--- scr/transformers.py
+++ scr/transformers.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import FunctionTransformer
-
+import numpy as np
 
 def select_features(df, features = None):
     # select features we want
@@ -22,6 +22,28 @@ def select_city(df,city=None):
     else:
         df_city = df.loc[city]
     return df_city
+
+def logtransform_target(target):
+    return np.log(target+1.)
+    
+def inverse_logtransform_target(transformed):
+    return np.exp(transformed)-1
+
+def create_min_yearly_temp(x):
+    if x.isnull().any().any()==True:
+        x = deal_with_missing_values(x)
+    x['min_yearly_temp']=x['station_min_temp_c'].rolling(53).min()
+    x['min_yearly_temp']=x['min_yearly_temp'].fillna(x['min_yearly_temp'].mean())
+    return x
+    
+def create_lagged_column(d,col_name,lag=1):
+    df=d.copy()
+    if df.isnull().any().any()==True:
+        df = deal_with_missing_values(df)
+    df[col_name+f'_lagged_{lag:1.2f}']=df[col_name].shift(-lag)
+    df[col_name+f'_lagged_{lag:1.2f}'].fillna(method='ffill',inplace=True)
+    return df
+
 
 SelectCity = FunctionTransformer(func = select_city, check_inverse = False)
 Impute = FunctionTransformer(func = deal_with_missing_values, check_inverse = False)
